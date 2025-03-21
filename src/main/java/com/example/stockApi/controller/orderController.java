4fcc0662bebb909920859907c411ea6c412c8b40
이수진 <ascs25@naver.com>
2025-03-21 23:06:37 +0900

    0321 주식 주문 생성 controller 뼈대 작업

---
+++ src/main/java/com/example/stockApi/controller/orderController.java
@@ -0,0 +1,18 @@
+package com.example.stockApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class orderController {
+
+	@PostMapping
+	public void createOrder() {
+		return;
+	}
+
+}
---
+++ src/test/java/com/example/stockApi/controller/orderControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.stockApi.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(orderController.class)
+public class orderControllerTest {
+	
+	// 200
+	@Test
+	@Disabled
+	void shouldReturn200WhenGetOrders() {
+		
+	}
+	
+	// 200
+	@Test
+	@Disabled
+	void shouldReturn200WhenGetOrderById() {
+		
+	}
+}
